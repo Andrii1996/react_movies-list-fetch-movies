3f7fd1c5a48bb459ed2b55ce08634a0ae1087580
Andrii Medziak <you@example.com>
2020-11-16 15:26:20 +0200

    Updates

---
+++ precache-manifest.771953919afacff2c434a35a5a45557c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6748c53541da721d1a7cc6780b1271ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28048753f3e495e896aa",
+    "url": "./static/css/2.92730f33.chunk.css"
+  },
+  {
+    "revision": "5b23cd26cb9951e20344",
+    "url": "./static/css/main.9aaf0c5f.chunk.css"
+  },
+  {
+    "revision": "28048753f3e495e896aa",
+    "url": "./static/js/2.1916c730.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.1916c730.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b23cd26cb9951e20344",
+    "url": "./static/js/main.7bd7a868.chunk.js"
+  },
+  {
+    "revision": "aae0386031766cdb5520",
+    "url": "./static/js/runtime-main.63136dd4.js"
+  }
+]);
